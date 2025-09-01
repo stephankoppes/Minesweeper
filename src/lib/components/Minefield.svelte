fe023748eb4fb49f7fc8194ed7a6735979e312af
Stephan Koppes <stephan.koppes@live.nl>
2025-09-01 23:23:19 +0200

    First setup

---
+++ src/lib/components/Minefield.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    let fieldCols = 8;
+    let fieldRows = 8;
+</script>
+
+<h3>Minefield</h3>
+
+<div class="tbl">
+    {#each { length: fieldRows }, row}
+        <div class="row">
+            {#each { length: fieldCols }, col}
+                <div class="cell">{row}-{col}</div>
+            {/each}
+        </div>
+    {/each}
+</div>
+
+<style>
+.tbl {
+    display: flex;
+    flex-direction: column;
+}
+
+.row {
+    display: flex;
+    height: 60px;
+    justify-content: center;
+}
+
+.cell {
+    display: flex;
+    width: 60px;
+    border: 1px solid blue;
+    background-color: cornflowerblue;
+    color: white;
+    align-items: center;
+    justify-content: center;
+}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import Minefield from "$lib/components/Minefield.svelte";
+</script>
+
+<Minefield />
